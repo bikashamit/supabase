acde8942518924302d6e866649dd9927e8e17193
amit kumar <amit.gangaur@gmail.com>
2023-08-16 11:28:16 +0530

    hiding keys and credentials

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css'
 
-function App() {
-  return (
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
+
+
+const api_url=process.env.SUPABASE_API_URL;
+const bearer_key=process.SUPABASE_BEARER_KEY
+const supabase = createClient(api_url,bearer_key);
+
+
+export default function App() {
+  const [session, setSession] = useState(null)
+const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+     
+    })
+    const fetchTodos = async () => {
+      try {
+        const { data, error } = await supabase.from('todos').select('*');
+        if (error) {
+          throw error;
+        }
+        setTodos(data);
+      } catch (error) {
+        console.error('Error fetching todos:', error.message);
+      }
+    };
+
+    fetchTodos();
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      
+      setSession(session)
+    })
+
+    return () => subscription.unsubscribe()
+  }, [])
+  const  handleClick=()=> {
+    const signOutfunction=async ()=>{
+      const { error } = await supabase.auth.signOut()
+    }
+    signOutfunction()
+  }
+
+  if (!session) {
+    return (
+      <div>
+      <h1>Supabase Realtime App</h1>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.task}</li>
+        ))}
+      </ul>
+      <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />
+      </div>
+      )
+  }
+  
+   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Supabase Realtime App</h1>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.task}</li>
+        ))}
+      </ul>
+      <a onClick={handleClick}>signout</a>
     </div>
   );
 }
-
-export default App;
